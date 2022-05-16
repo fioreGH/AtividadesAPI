ba09824641333653426aef30b321d66781a67a99
Raphael Fiore <raphafiore@live.com>
2022-05-16 14:14:17 -0300

    Todos os métodos atualizados! Minimal API em funcionamento, com Program bem enxuto

--- AtividadesAPI/AtividadesAPI/EndPoints/AtividadesEndpoints.cs
+++ AtividadesAPI/AtividadesAPI/EndPoints/AtividadesEndpoints.cs
@@ -21,6 +21,45 @@ namespace AtividadesAPI.EndPoints
                 return Results.Ok(atividades);
             
             });
+
+            app.MapGet("/Atividades/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                /*var atividades = con.Get<Atividade>(id);
+
+                if (atividades is null)
+                    return Results.BadRequest();
+
+                return Results.Ok(atividades);*/
+
+                return con.Get<Atividade>(id)  is Atividade atividade ?  Results.Ok(atividade) : Results.BadRequest("Item não encontrado!");
+
+            });
+
+            app.MapPost("/Atividades", async (GetConnection connectionGetter, Atividade Atividade) => 
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(Atividade);
+                return Results.Created($"/Atividade/{id}", Atividade);
+            });
+
+            app.MapPut("/Atividades", async (GetConnection connectionGetter, Atividade Atividade) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(Atividade);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/Atividades/{id}", async (GetConnection connectionGetter, int id) => 
+            {
+                using var con = await connectionGetter();
+                var excluir = con.Get<Atividade>(id);
+                if (excluir is null)
+                    return Results.NotFound("Item não encontrado!");
+
+                con.Delete(excluir);
+                return Results.Ok(excluir);
+            });
         }
     }
 }
